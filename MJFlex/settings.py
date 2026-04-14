06bd4bbc0838db6f7c6d5b04982d0fd222cccd53
Munesh J <muneshj32@gmail.com>
2026-04-14 19:25:58 +0530

    add gitignore and remove secrets

--- MJFlex/settings.py
+++ MJFlex/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1k9sylbl+#jfsaf^)m@-i8$ckv=%buyj33m-1)zj0rb6^^^gy)"
+SECRET_KEY =  "old_key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
