4a272c4e836910b60eae027e9ad6f93843aeb227
Munesh J <muneshj32@gmail.com>
2026-06-12 13:36:19 +0530

    Add is_verified field to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Verification', {'fields': ('is_verified',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
